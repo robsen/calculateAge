43b0af6f18e4faaf17d19af0f363369ae0e2d9a8
Robert Jaskowski <robert.jaskowski@ymail.com>
2018-03-27 23:57:08 +0200

    Add index.php as HTML5 skeleton with page structure
    Form for birthyear input included.

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Age Calculation</title>
+        <meta charset="UTF-8">
+
+        <meta name="author" content="Robert Jaśkowski">
+        <meta name="description" content="Calculating imprecise age by given birthyear">
+    </head>
+    <body>
+        <h1>Age Calculation</h1>
+
+        <form method="POST" action="age.php">
+            <label for="birthyear">Year of Birth:</label>
+            <input type="text" id="birthyear" name="birthyear">
+            <button type="submit">Calculate</button>
+        </form>
+    </body>
+</html>
