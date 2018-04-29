77a5bbdc91db426a1960125d0e424642458aecee
Robert Jaskowski <robert.jaskowski@ymail.com>
2018-04-29 17:39:25 +0200

    Implement Better Represention of Tiny Ages
    Closes todo issue #8 completely.

--- python/age.py
+++ python/age.py
@@ -18,8 +18,20 @@ def main():
 	today = date.today()
 	
 	ageInDays = (today - dateOfBirth).days
-	ageInYears = int(ageInDays / 365)
-	print("Age =", ageInYears, "years")
+	if (ageInDays < 32):
+		print("Age =", ageInDays, "days")
+	elif (ageInDays < 366):
+		ageInMonths = int(ageInDays / 30)
+		ageInDays = ageInDays - (ageInMonths * 30)
+		print(
+			"Age =",
+			ageInMonths,
+			"months",
+			ageInDays,
+			"days")
+	else:
+		ageInYears = int(ageInDays / 365)
+		print("Age =", ageInYears, "years")
 
 
 # run this module only directly
